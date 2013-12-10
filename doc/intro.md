ef6e1aa122c9651afa302af601489e342ec1e2ea
tthieman <travis.thieman@gmail.com>
2013-12-10 18:36:21 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-zulip
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_zulip/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-zulip.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-zulip.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
